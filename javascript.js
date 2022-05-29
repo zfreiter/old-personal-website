3ac1f3253f867c840ab07ba9571389d9fd4bee90
Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
2022-05-28 21:14:51 -0700

    navbar
    Created the outline to the navbar with html, CSS, and JavaScript.

---
+++ javascript.js
@@ -0,0 +1,19 @@
+const bars = document.querySelector("#bars-open");
+const x = document.querySelector("#x-close");
+const navItems = document.querySelectorAll(".nav-item");
+
+bars.onclick = function () {
+  bars.className += "fa-solid fa-bars fa-2x hide";
+  x.className += "fa-solid fa-xmark fa-2x show";
+  navItems.forEach((item) => {
+    item.className += " show";
+  });
+};
+
+x.onclick = function () {
+  bars.className = "fa-solid fa-bars fa-2x";
+  x.className = "fa-solid fa-xmark fa-2x";
+  navItems.forEach((item) => {
+    item.className = "nav-item";
+  });
+};
