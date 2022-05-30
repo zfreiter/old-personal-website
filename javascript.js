c3a17e0392a0ee4e2274f00c53d2548330043339
Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
2022-05-29 21:25:58 -0700

    Form creation
    Did the form outline.  Used grid to construct it.  Added some color scheme to the project, but need to had var for the colors of the project.

--- javascript.js
+++ javascript.js
@@ -1,7 +1,12 @@
+// elements to add function to navbar
 const bars = document.querySelector("#bars-open");
 const x = document.querySelector("#x-close");
 const navItems = document.querySelectorAll(".nav-item");
 
+// element to clear the submit button
+const form = document.getElementById("contact-form");
+
+// Opens the navbar
 bars.onclick = function () {
   bars.className += "fa-solid fa-bars fa-2x hide";
   x.className += "fa-solid fa-xmark fa-2x show";
@@ -10,12 +15,20 @@ bars.onclick = function () {
   });
 };
 
+// Closes the navbar
 x.onclick = function () {
   bars.className = "fa-solid fa-bars fa-2x";
   x.className = "fa-solid fa-xmark fa-2x";
-  const addClass = ["one", "two", "three"];
+  const addClass = ["one", "two", "three", "four"];
 
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 4; i++) {
     navItems[i].className = "nav-item " + addClass[i];
   }
 };
+
+// Resets the form
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  form.reset();
+});
