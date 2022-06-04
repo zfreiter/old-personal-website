262dd20a9108f3798f301a8cbc11f52f39d13a20
Zachary Freiter <31320978+zfreiter@users.noreply.github.com>
2022-06-04 14:02:37 -0700

    Slideshow work
    Working on the slideshow and using the w3school guide to do it.  Need to figure out why it is not resizing.

--- javascript.js
+++ javascript.js
@@ -47,3 +47,52 @@ form.addEventListener("submit", function (event) {
 
   form.reset();
 });
+
+// Slideshow code
+let slideIndex = 1;
+showSlides(slideIndex);
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const dotOne = document.querySelector(".dot-one");
+const dotTwo = document.querySelector(".dot-two");
+
+prev.addEventListener("click", function () {
+  plusSlides(-1);
+});
+next.addEventListener("click", function () {
+  plusSlides(1);
+});
+dotOne.addEventListener("click", function () {
+  currentSlide(1);
+});
+dotTwo.addEventListener("click", function () {
+  currentSlide(2);
+});
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.querySelectorAll(".projectSlides");
+  let dots = document.querySelectorAll(".dots");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
